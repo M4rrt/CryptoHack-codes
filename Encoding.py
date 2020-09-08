4e9e065e2675084834b12975397f27ff7d53f5e2
Guilherme Martins <67018713+M4rrt@users.noreply.github.com>
2020-09-08 08:19:38 -0300

    Docstrings update
    making a better docstrings

--- Encoding.py
+++ Encoding.py
@@ -3,25 +3,25 @@ import base64 , codecs
 import Decoding as DC
 
 
-# encode a string to ascii table
+# Encodes the String 'original'  with ascii table
 def ascii_encode(original):
     encodede = [ord(i)for i in original]
     return encodede
 
 
-# encode a string into a Hex string
+# Encodes the String 'original' whith Hex
 def hex_encode(original):
     return original.encode().hex()
 
     
-# encode the string into a Hex string with 0x in the start of it 
+# Encodes the String 'original' with Hex putting '0x' in the start of it 
 def Hex_0x_encode(original):
     return hex(bytes_to_long(original.encode()))
 
-# encode a string into base64 
+# Encodes the String 'original' with base64 
 def base64_encode(original):
     return base64.b64encode(original.encode())
 
-# takes a string and encode it in rot_13
+# Encodes the String 'original' with rot_13
 def rot_13_encode(original):
     return codecs.encode(original,"rot_13")
